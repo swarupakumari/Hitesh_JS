21c21df77a3da0410821df8374ee7aded38d93b9
Swarupa Kumari <151866378+swarupakumari@users.noreply.github.com>
2025-04-22 11:21:31 +0000

    first commit to check

---
+++ 01_Basics/test.js
@@ -0,0 +1,9 @@
+const id =12345;  // you cant reintialised const here
+let ename="Swarupa";
+//make sure you dont use var because it doesnot able block scope as well as functional scope so we prefer let
+//anything in clury braces is called as scope
+lastname="singh";//JavaScript will implicitly create a global variable.implicitly creates a global variable lastname,
+//  attached to the window object (in browsers).
+console.log(lastname);
+
+console.table([id,ename,lastname]);
