3ad6a631fae63cd1c3624565027aa3500bb83067
Swarupa Kumari <151866378+swarupakumari@users.noreply.github.com>
2025-04-25 06:43:14 +0000

    string

---
+++ 01_Basics/string.js
@@ -0,0 +1,26 @@
+
+const name='swarupa';
+let val=1234;
+console.log(` my name is ${name}.The value is ${val} `); 
+const emp=new String("xyz")
+console.log(emp.charAt(2));
+console.log(emp.indexOf('y'));
+const x =emp.substring(0,2);
+console.log(x);
+const r=emp.slice(-8,4);
+console.log(r);
+let l="  Hitesh  ";
+console.log(l.trim());
+const url ="https://www.google%20.co.in/"
+console.log(url.replace('%20','-'));
+console.log(url);
+console.log(url.includes('Sundar'));
+console.log(url.split('.'));
+
+
+
+
+
+
+
+
